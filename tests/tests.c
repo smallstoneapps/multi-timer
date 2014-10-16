363961f196a000425bc49729aeefbbb902d234b3
Matthew Tole <matthewtole@gmail.com>
2014-10-16 10:45:58 -0700

    Minor tweak to the test suite

--- tests/tests.c
+++ tests/tests.c
@@ -40,16 +40,6 @@ tests/tests.c
 #include "groups.h"
 #include "../src/generated/appinfo.h"
 
-// Colour code definitions to make the output all pretty.
-#define KNRM  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KMAG  "\x1B[35m"
-#define KCYN  "\x1B[36m"
-#define KWHT  "\x1B[37m"
-
 // Keep track of how many tests have run, and how many have passed.
 int tests_run = 0;
 int tests_passed = 0;
