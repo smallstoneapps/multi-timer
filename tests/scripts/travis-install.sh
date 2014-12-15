62c1b9ffdebeb782e8ac0edf034bc869b30822a5
Matthew Tole <matthewtole@gmail.com>
2014-12-15 00:05:08 +0000

    Add Travis install script to enable tests to run

---
+++ tests/scripts/travis-install.sh
@@ -0,0 +1,12 @@
+SDK_VERSION=2.8.1
+PACKAGE_NAME=PebbleSDK-$SDK_VERSION
+SDK_INCLUDE_FOLDER=$PACKAGE_NAME/Pebble/include
+INCLUDE_FOLDER=tests/include
+SDK_ZIP_NAME=$PACKAGE_NAME.tar.gz
+
+wget http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/sdk2/$SDK_ZIP_NAME
+tar -zxf $SDK_ZIP_NAME
+mv $SDK_INCLUDE_FOLDER/pebble.h $INCLUDE_FOLDER
+mv $SDK_INCLUDE_FOLDER/pebble_fonts.h $INCLUDE_FOLDER
+mv $SDK_INCLUDE_FOLDER/pebble_app_info.h $INCLUDE_FOLDER
+touch $INCLUDE_FOLDER/pebble_warn_unsupported_functions.h
